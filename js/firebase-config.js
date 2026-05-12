b21c2d9396f19c42dd8a843eb3227b7677c66f8c
gerriodailmypratama-prog <gerriodailmypratama@gmail.com>
2026-05-13 02:44:39 +0700

    Add OFFICE_LOCATION constant for hybrid geofence

--- js/firebase-config.js
+++ js/firebase-config.js
@@ -4,19 +4,27 @@ import { getFirestore } from "https://www.gstatic.com/firebasejs/10.12.0/firebas
 import { getStorage } from "https://www.gstatic.com/firebasejs/10.12.0/firebase-storage.js";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyAtbQWVdMYzIhp0hdtioPaDmULEYQBYvmg",
-  authDomain: "absensi-karyawan-207d9.firebaseapp.com",
-  projectId: "absensi-karyawan-207d9",
-  storageBucket: "absensi-karyawan-207d9.firebasestorage.app",
-  messagingSenderId: "819005025283",
-  appId: "1:819005025283:web:459c4dba62bb22dcc63236",
-  measurementId: "G-35DB38FN9P"
+    apiKey: "AIzaSyAtbQWVdMYzIhp0hdtioPaDmULEYQBYvmg",
+    authDomain: "absensi-karyawan-207d9.firebaseapp.com",
+    projectId: "absensi-karyawan-207d9",
+    storageBucket: "absensi-karyawan-207d9.firebasestorage.app",
+    messagingSenderId: "819005025283",
+    appId: "1:819005025283:web:459c4dba62bb22dcc63236",
+    measurementId: "G-35DB38FN9P"
 };
 
 // Email OWNER (Dashboard owner access)
 export const OWNER_EMAILS = [
-  "gerriomail@gmail.com"
-];
+    "gerriomail@gmail.com"
+  ];
+
+// Lokasi kantor/ruko (Ruko BSM A2/9, Pakulonan, Serpong Utara)
+// Hybrid geofence: absen tetap boleh, tapi diberi flag kalau di luar radius.
+export const OFFICE_LOCATION = {
+    lat: -6.238929,
+    lng: 106.6459816,
+    radiusMeters: 150
+};
 
 const app = initializeApp(firebaseConfig);
 export const auth = getAuth(app);
