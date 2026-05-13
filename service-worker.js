22a7365282dbce3b7a1c7dc0db88e77b53f459b1
gerriodailmypratama-prog <gerriodailmypratama@gmail.com>
2026-05-13 16:55:00 +0700

    chore(sw): bump cache v10 -> v11

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,4 @@
-const CACHE = 'absensi-v10';const ASSETS = [
+const CACHE = 'absensi-v11';const ASSETS = [
     './','./index.html','./karyawan.html','./owner.html',
     './css/style.css','./manifest.json','./icon.svg'
 ];
