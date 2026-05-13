c954664b66e3f435c84ba1e6b1f3423b2d4062e6
gerriodailmypratama-prog <gerriodailmypratama@gmail.com>
2026-05-14 02:16:35 +0700

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,4 @@
-const CACHE = 'absensi-v20';const ASSETS = [
+const CACHE = 'absensi-v21';const ASSETS = [
     './','./index.html','./karyawan.html','./owner.html',
     './css/style.css','./manifest.json','./icon.svg'
 ];
