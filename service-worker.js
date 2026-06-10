ea49e2aa37ca0a6d723f5e1aa7b5192169381540
gerriodailmypratama-prog <gerriodailmypratama@gmail.com>
2026-06-11 01:04:03 +0700

    chore: bump cache v37 -> v38 supaya fix edit lembur kebawa

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,4 @@
-const CACHE = 'absensi-v37';const ASSETS = [    './','./index.html','./karyawan.html','./owner.html',
+const CACHE = 'absensi-v38';const ASSETS = [    './','./index.html','./karyawan.html','./owner.html',
     './css/style.css','./manifest.json','./icon.svg'
 ];
 
